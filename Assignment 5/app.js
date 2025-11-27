5b4abb555e2f522d01e2ea29505abf60979f0730
M-Hanzala <hhanzala419@gmail.com>
2025-11-27 10:48:45 +0500

    Assignment 5 Completed

--- Assignment 5/app.js
+++ Assignment 5/app.js
@@ -87,22 +87,105 @@
 // document.write(celsiusTemprature + "F is " + C + "C." + "<br><br>")
 // document.write(fahrenheitTemprature + "C is " + F + "F.")
 
-// Question 7
-document.write("Shooping Cart" + "<br><br><br>")
+// // Question 7
+// document.write("Shooping Cart" + "<br><br><br>")
 
-var item1Price = 650
-var item2Price = 100
-var qtyOfItem1 = 3
-var qtyOfItem2 = 7
-var shippingCharges = 150
+// var item1Price = 650
+// var item2Price = 100
+// var qtyOfItem1 = 3
+// var qtyOfItem2 = 7
+// var shippingCharges = 150
+
+// document.write(
+//     "Price of item1 is " + item1Price + "<br>" +
+//     "Quantity of item1 is " + qtyOfItem1 + "<br>" +
+//     "Price of item2 is " + item2Price + "<br>" +
+//     "Quantity of item2 is " + qtyOfItem2 + "<br>" +
+//     "Shipping Charges " + shippingCharges + "<br><br>"
+// )
+
+// var totalCost = (item1Price*qtyOfItem1) + (item2Price*qtyOfItem2) + (shippingCharges)
+// document.write("Total cost of your order is " + totalCost) 
+
+
+// // Question 8
+// var totalMarks = 550
+// var obtainedMarks = prompt("Enter Your Obtained Marks...\nOutof 550:")
+// var percentage = obtainedMarks*100/totalMarks
+
+// document.write(
+//     "Total Marks : " + totalMarks + "<br>" +
+//     "Obtained Marks : " + obtainedMarks + "<br><br>" +
+//     "Your Percentage Is : " + percentage + "%"
+// )
+
+
+// // Question 9
+// document.write("Currency into PKR")
+
+// var usd = prompt("Enter Your US Dollar Amount :")
+// var sar = prompt("Enter Your Saudi Riyal Amount :")
+
+// // Exchange Rates
+// var usdToPkr = usd*282.57;
+// var sarToPkr = sar*75.17; 
+// var totalCurrencyInPkr = usdToPkr+sarToPkr
+
+// document.write(
+//     "Your US Dollar Into Pkr = " + usdToPkr + "<br>" +
+//     "Your Saudi Riyal Into Pkr = " + sarToPkr + "<br><br>" +
+
+//     "Total Currency In Pkr = " + totalCurrencyInPkr
+// )
+
+
+// // Question 10
+// var initValue = 3
+// var doingArithemetic = initValue + 5 * 10 / 2
+
+// // Question 11
+// document.write("Age Calculator" + "<br><br>")
+
+// var currentYear = prompt("Enter Current Year :")
+// var birthYear = prompt("Enter Your Birth Year :")
+// var ageIs = currentYear - birthYear
+
+// document.write(
+//     "Current Year Is : " + currentYear + "<br>" +
+//     "Birth Year Is : " + birthYear + "<br><br>" +
+    
+//     "Your Age Is : " + ageIs
+// )
+
+// // Question 12
+// document.write("The Geometrizer" + "<br><br>")
+
+// var r = 20
+// var circumOfCircle = 2*(3.142)*r
+// var areaOfCircle = (3.142)*r*2
+
+// document.write(
+//     "Radius Of Circle : " + r + "<br>" +
+//     "The Circumference is : " + circumOfCircle + "<br>" +
+//     "The Area is : " + areaOfCircle 
+// )
+
+
+// Question 13
+document.write("The Lifetime Supply Calculator" + "<br><br>")
+
+var favSnack = "Unknown"
+var currentAge = 18
+var maxAge = 65
+var estAmountOfPerDay = 3
+var daysInYear = 365
+
+var calculatingStuff = estAmountOfPerDay*daysInYear*(maxAge-currentAge)
 
 document.write(
-    "Price of item1 is " + item1Price + "<br>" +
-    "Quantity of item1 is " + qtyOfItem1 + "<br>" +
-    "Price of item2 is " + item2Price + "<br>" +
-    "Quantity of item2 is " + qtyOfItem2 + "<br>" +
-    "Shipping Charges " + shippingCharges + "<br><br>"
+    "Favourite Snack : " + favSnack + "<br>" +
+    "Current Age : " + currentAge + "<br>" +
+    "Estimated Max Age : " + maxAge + "<br>" +
+    "Amount Of Snack Per Day : " + estAmountOfPerDay + "<br><br>" +
+    "You will need " + calculatingStuff + " snack to last you until the ripe old age of " + maxAge
 )
-
-var totalCost = (item1Price*qtyOfItem1) + (item2Price*qtyOfItem2) + (shippingCharges)
-document.write("Total cost of your order is " + totalCost)
