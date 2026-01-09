852abcec3b68e0804d61cb71494012c7323b5674
M-Hanzala <hhanzala419@gmail.com>
2026-01-09 16:43:54 +0500

    Assignment 11 Created

---
+++ Assignment 11/app.js
@@ -0,0 +1,101 @@
+// // Question 1
+// var firstName = prompt("Enter First Name:");
+// var lastName = prompt("Enter Last Name:");
+// var fullName = firstName + lastName;
+// alert("Welcome " + fullName);
+
+
+// // Question 2
+// var favMobile = prompt("Enter your favourite mobile phone:");
+// document.write(
+//     "My favourite phone is: " + favMobile + "<br>" +
+//     "Length of strings: " + favMobile.length
+// );
+
+
+// // Question 3
+// var nationality = "Pakistani";
+// document.write(
+//     "String: " + nationality + "<br>" +
+//     "Index of 'n': " + nationality.indexOf('n')
+// );
+
+
+// // Question 4
+// var greet = "Hello World";
+// document.write(
+//     "String: " + greet + "<br>" +
+//     "Last index of 'l': " + greet.lastIndexOf('l')
+// );
+
+
+// Question 5
+// var nationality = "Pakistani";
+// document.write(
+//     "String: " + nationality + "<br>" +
+//     "Char at Index 3: " + nationality.charAt(3)
+// );
+
+
+// // Question 6
+// var firstName = prompt("Enter First Name:");
+// var lastName = prompt("Enter Last Name:");
+// var fullName = firstName.concat(" ", lastName);
+// alert("Welcome " + fullName);
+
+
+// // Question 7
+// var city = "Hyderabad";
+// document.write(
+//     "City: " + city + "<br>" +
+//     "After replacement: " + city.replace("Hyder" , "Islam")
+// );
+
+
+// // Question 8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var replace = message.replace(/and/g , "&");
+// document.write(
+//     "String: " + message + "<br>" +
+//     "After replacement: " + replace
+// );
+
+
+// // Question 9
+// var str = "472";
+// console.log(str , typeof(str));
+
+// var num = +str; 
+// console.log(num , typeof(num));
+
+
+// // Question 10
+// var input = prompt("Enter any lowercase input:");
+// document.write(
+//     "User Input: " + input + "<br>" +
+//     "Upper Case: " + input.toUpperCase()
+// );
+
+
+// // Question 11
+// var input = prompt("Enter any Character:");
+
+// var firstChar = input[0].toUpperCase();
+// var restChars = input.slice(1).toLowerCase();
+
+// var titleCase = firstChar + restChars;
+
+// document.write(
+//     "User Input: " + input + "<br>" +
+//     "Title Case: " + titleCase
+// );
+
+
+// // Question 12
+// var num = 35.36;
+// var str = num.toString();
+// var str = str.replace("." , "");
+// document.write(
+//     "Number: " + num + "<br>" +
+//     "Result: " + str
+// );
