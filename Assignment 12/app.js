9b74ebd767e0971058b6e2cf2a6fbb88cd350ae9
M-Hanzala <hhanzala419@gmail.com>
2026-01-13 15:18:57 +0500

    Assignment 12

---
+++ Assignment 12/app.js
@@ -0,0 +1,58 @@
+// // Question 1
+// var pNumber = +prompt("Enter a positive decimal value:");
+
+// document.write(
+//     "Number: " + pNumber + "<br>" +
+//     "Round off value: " + Math.round(pNumber) + "<br>" +
+//     "Floor value: " + Math.floor(pNumber) + "<br>" +
+//     "Ceil value: " + Math.ceil(pNumber)
+// );
+
+
+// // Question 2
+// var nNumber = +prompt("Enter a negitive decimal value:");
+
+// document.write(
+//     "Number: " + nNumber + "<br>" +
+//     "Round off value: " + Math.round(nNumber) + "<br>" +
+//     "Floor value: " + Math.floor(nNumber) + "<br>" +
+//     "Ceil value: " + Math.ceil(nNumber)
+// );
+
+
+// // Question 3
+// var num = -4;
+// document.write(
+//     "The absolute value of " + num + " is " + Math.abs(num)
+// );
+
+
+// // Question 4
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// console.log("Random Dice Value --> " + diceValue);
+
+
+// // Question 5
+// var randomValue = Math.floor(Math.random() * 2) + 1;
+// var toss;
+// if (randomValue == 2) {
+//     toss = "Heads";
+// } else if (randomValue == 1) {
+//     toss = "Tails";
+// }
+
+// document.write(
+//     randomValue + "<br>" +
+//     "Random Coin Value: " + toss
+// )
+
+
+// // Question 6
+// var number = Math.floor(Math.random() * 100) + 1;
+// document.write("Random number between 1 and 100: " + number);
+
+
+// // Question 7
+// var userWeight = prompt("Enter your weight in kilograms: ");
+// userWeight = parseFloat(userWeight);
+// document.write("The weight of user is " + userWeight + " kilograms.");
