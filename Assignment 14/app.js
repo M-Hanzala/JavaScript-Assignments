3232ccf9bde86b1049f9033a7dc54187315015bf
M-Hanzala <hhanzala419@gmail.com>
2026-01-14 15:47:37 +0500

    Assignment 14 Created

---
+++ Assignment 14/app.js
@@ -0,0 +1,23 @@
+// Question 1
+function currentDate() {
+    var date = new Date();
+    return date;
+}
+
+
+// Question 2
+function greetUser() {
+    var firstName = prompt("Enter your first name:");
+    var lastName = prompt("Enter your last name:");
+    return alert("WELCOME. " + firstName + " " + lastName);
+}
+
+
+// Question 3
+function add2Num(a, b) {
+    var sum = a + b;
+    return sum;
+}
+
+
+// Question 4
