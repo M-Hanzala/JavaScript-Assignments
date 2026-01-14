8a14d1901f36c24a6d5438ff1f6f1cdccbe76000
M-Hanzala <hhanzala419@gmail.com>
2026-01-14 16:46:20 +0500

    Assignment 14 Question 7 Done

--- Assignment 14/app.js
+++ Assignment 14/app.js
@@ -20,4 +20,56 @@ function add2Num(a, b) {
 }
 
 
-// Question 4
+// // Question 4
+// function calculator(num1, num2, operator) {
+//     if (operator === "+") {
+//         return num1 + num2;
+//     } else if (operator === "-") {
+//         return num1 - num2;
+//     } else if (operator === "*") {
+//         return num1 * num2;
+//     } else if (operator === "/") {
+//         return num1 / num2;
+//     } else {
+//         return "Invalid Operator";
+//     }
+// }
+
+// var opt = prompt("Enter Operator For Calculate:" + "\n+   ,   -   ,   *   ,   /");
+// var n1 = +prompt("Enter first value:");
+// var n2 = +prompt("Enter second value:");
+// var result = calculator(n1, n2, opt);
+// document.write(result);
+
+
+// // Question 5
+// function square(val) {
+//     var results = val * val;
+//     return results;
+// }
+// var value = 4;
+// console.log(square(value));
+
+
+// // Question 6
+// function factorial(num) {
+//     var results = 1;
+//     for (var i = 1; i <= num; i++) {
+//         results = results * i;
+//     }
+//     return results;
+// }
+// var facNum = 5;
+// console.log(factorial(facNum));
+
+
+// // Question 7
+// function counting(start, end) {
+//     for (var i = start; i <= end; i++) {
+//         results = console.log(i);
+//     }
+//     return results;  
+// }
+// var countStart = +prompt("Enter starting point of counting:");
+// var countEnd = +prompt("Enter ending point of counting:");
+// counting(countStart, countEnd);
