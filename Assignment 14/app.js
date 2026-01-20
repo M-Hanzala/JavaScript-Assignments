f74e5d3cc5f221d2b785400f957e9aa880ae98da
M-Hanzala <hhanzala419@gmail.com>
2026-01-20 17:02:44 +0500

    Assignment 14 Question 12 Done

--- Assignment 14/app.js
+++ Assignment 14/app.js
@@ -73,3 +73,84 @@ function add2Num(a, b) {
 // var countStart = +prompt("Enter starting point of counting:");
 // var countEnd = +prompt("Enter ending point of counting:");
 // counting(countStart, countEnd);
+
+
+// // Question 8
+// function calculateHypotenuse(base, perpendicular) {
+//     function calculateSquare(num) {
+//         return num * num;
+//     }
+//     var hyp2 = calculateSquare(base) + calculateSquare(perpendicular);
+//     var hyp = Math.sqrt(hyp2);
+//     return console.log("Hypotenuse = " + hyp);
+// }
+
+// var b = prompt("Enter base: ");
+// var p = prompt("Enter perpendicular: ");
+// calculateHypotenuse(b, p);
+
+
+// // Question 9
+// function calculateArea(width, height) {
+//     var area = width * height;
+//     return console.log(area);
+// }
+
+// calculateArea(5, 10);
+
+// var w = 5;
+// var h = 10;
+// calculateArea(w, h);
+
+
+// // Question 10
+// function isPalindrome(string) {
+//     var reverse = "";
+//     for (var i = string.length - 1; i >= 0; i--) {
+//         reverse += string[i];
+//     }
+//     if (string === reverse) {
+//         return console.log("Given string is Palindrome");
+//     } else {
+//         return console.log("Given string is not Palindrome");
+//     }
+// }
+
+// var str = "madam";
+// isPalindrome(str);
+
+
+// // Question 11
+// function titleCase(string) {
+//     var words = string.toLowerCase().split(" ");
+//     var results = "";
+
+//     for (var i = 0; i < words.length; i++) {
+//         results += words[i][0].toUpperCase() + words[i].slice(1) + " ";
+//     }
+//     return results;
+// }
+
+// var str = "my name is hanzala";
+// console.log(
+//     titleCase(str)
+// );
+
+
+// // Question 12
+// function longestWord(sentence) {
+//     var words = sentence.split(" ");
+//     var longestWord = "";
+
+//     for (var i = 0; i < words.length; i++) {
+//         if (words[i].length > longestWord.length) {
+//             longestWord = words[i];
+//         }
+//     }
+//     return longestWord;
+// }
+
+// var str = "Web Development Tutorial";
+// console.log(
+//     longestWord(str)
+// );
