a9f110dd1fee2d1ffb53ee7ac182f226c7ad2426
M-Hanzala <hhanzala419@gmail.com>
2026-01-21 17:08:38 +0500

    Assignment 15 Created

---
+++ Assignment 15/app.js
@@ -0,0 +1,21 @@
+// // Question 1
+// function power(a, b) {
+//     var pow = Math.pow(a, b);
+//     return pow;
+// }
+// console.log(
+//     power(5, 10)
+// );
+
+
+// // Question 2
+// function detLeapYear(year) {
+//     if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+//         alert(year + " is a leap year");
+//     } else {
+//         alert(year + " is not a leap year");
+//     }
+// }
+
+// var y = +prompt("Enter Year:");
+// detLeapYear(y);
